d194d379a71434f76ba6da4277550552c3004fe7
uch2ha <sinyavskiy.d@gmail.com>
2022-02-04 14:25:04 +0200

    add Pressure class

---
+++ Models/Pressure.cs
@@ -0,0 +1,13 @@
+namespace MarsWeatherApi.Models
+{
+    public class Pressure
+    {
+        public int Id { get; set; }
+        public float Average { get; set; }
+        public float Minimum { get; set; }
+        public float Maximum { get; set; }
+        
+      //   public int Sol_id { get; set; }
+
+    }
+}
