82b9fe1bfc0b00ce26d73c3534c5dea5ce6647ed
Heta Björklund <heta.bjorklund@gmail.com>
2022-02-09 12:43:10 +0200

    Registered ApplicationDBContext

--- Program.cs
+++ Program.cs
@@ -1,26 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using MarsWeatherApi.Models;
+using MarsWeatherApi.Contexts;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 
-// This is for the Sol class & SolContext
-builder.Services.AddDbContext<SolContext>(opt =>
-    opt.UseInMemoryDatabase("SolList"));
-
-// This is for the Pressure class & Pressure Context
-builder.Services.AddDbContext<PressureContext>(opt =>
-    opt.UseInMemoryDatabase("PressureList"));
-
-// This is for the Wind class & Wind Context
-builder.Services.AddDbContext<WindContext>(opt =>
-    opt.UseInMemoryDatabase("WindList"));
-
-// This is for the Temperature class & TemperatureContext
-builder.Services.AddDbContext<TemperatureContext>(opt =>
-    opt.UseInMemoryDatabase("TemperatureList"));
+// Add database Context
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+    opt.UseInMemoryDatabase("MarsWeatherApiDB"));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
