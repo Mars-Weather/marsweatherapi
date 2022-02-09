67186cdaab775f0c57f84627c9e6c0d95ca0e7bb
Heta Björklund <heta.bjorklund@gmail.com>
2022-02-09 12:44:53 +0200

    Added sol IDs to other classes, deleted unneeded Contexts

--- Models/Pressure.cs
+++ Models/Pressure.cs
@@ -5,9 +5,8 @@ namespace MarsWeatherApi.Models
         public int Id { get; set; }
         public float Average { get; set; }
         public float Minimum { get; set; }
-        public float Maximum { get; set; }
-        
-      //   public int Sol_id { get; set; }
+        public float Maximum { get; set; }        
+        public int Sol_Id { get; set; }
 
     }
 }
