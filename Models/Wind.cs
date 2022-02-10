38b4f71262ae37dcb18a9ee4f5d1ef95ae0b6dc3
Heta Björklund <heta.bjorklund@gmail.com>
2022-02-10 17:28:35 +0200

    FK linking with Sol id works, no object cycle
    Attributes still default to null?

--- Models/Wind.cs
+++ Models/Wind.cs
@@ -1,12 +1,15 @@
+using Newtonsoft.Json;
+
 namespace MarsWeatherApi.Models
 {
     public class Wind
     {
         public int Id { get; set; }
-        public float average { get; set; }
-        public float minimum { get; set; }
-        public float maximum { get; set; }
-        public string? mostCommonDirection { get; set; }
-        int sol_id { get; set; }
+        public float Average { get; set; }
+        public float Minimum { get; set; }
+        public float Maximum { get; set; }
+        public string? MostCommonDirection { get; set; }
+        [JsonIgnore] public Sol? Sol { get; set; }
+        public int SolForeignKey { get; set; }
     }
 }
