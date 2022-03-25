91d34f752fb697145354def3c9a7f160de5f21e2
Cho-ui <christian.lindell@myy.haaga-helia.fi>
2022-03-25 10:17:54 +0200

    DbUpdateService first refactor

--- DbUpdateService.cs
+++ DbUpdateService.cs
@@ -1,6 +1,5 @@
 using MarsWeatherApi.Contexts;
 using MarsWeatherApi.Models;
-using System.Text.Json;
 using System.Text.Json.Nodes;
 
 namespace MarsWeatherApi
@@ -9,7 +8,13 @@ namespace MarsWeatherApi
     {
         private readonly ILogger<DbUpdateService> _logger;
         private readonly IServiceProvider _serviceProvider;
-
+        private readonly string[] _assetPaths = 
+        {"./assets/week1_sols100-106.json",
+        "./assets/week2_sols107-113.json",
+        "./assets/week3_sols114-120.json",
+        "./assets/week4_sols121-127.json",
+        "./assets/week5_sols128-134.json"
+        };
         public DbUpdateService(IServiceProvider serviceProvider, ILogger<DbUpdateService> logger)
         {
             _logger = logger;
@@ -20,76 +25,79 @@ namespace MarsWeatherApi
             // Execute the background task as long as the application is run/is manually stopped
             while (!stoppingToken.IsCancellationRequested) 
             {
-                // TODO: Possible refactoring for readability 
-
                 // creates a new scope for each run, so DbContext can be accessed from a singleton
-                using (var scope = _serviceProvider.CreateScope()) {
-
-                    var scopedService = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                    
-                    // gets all existing sol numbers in db and adds them to a list
+                using (var scope = _serviceProvider.CreateScope()) 
+                {
+                    var scopedService = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();      
+                    int latestSolNumber = 0;
                     var dbSolList = scopedService.Sols.ToList();
-                    List<int> dbSolNumbers = new List<int>();
-                    foreach(var dbsol in dbSolList)
+
+                    if (dbSolList.Count > 0)
                     {
-                        dbSolNumbers.Add(dbsol.SolNumber);
+                       var latestSol = dbSolList.MaxBy(s => s.SolNumber);
+                       latestSolNumber = latestSol.SolNumber;
                     }
-
-                    // Loop through files in assets
-                    string[] assetPaths = 
-                    {"./assets/week1_sols100-106.json",
-                    "./assets/week2_sols107-113.json",
-                    "./assets/week3_sols114-120.json",
-                    "./assets/week4_sols121-127.json",
-                    "./assets/week5_sols128-134.json"
-                    };
-
-                    foreach(var path in assetPaths)
+                    // Loops through files in assets, saves new Sol objects into DB
+                    foreach(var path in _assetPaths)
                     {
                         string jsonString = File.ReadAllText(path);
-
                         JsonNode marsWeekNode = JsonNode.Parse(jsonString)!;
-
                         JsonArray marsSolKeys = marsWeekNode!["sol_keys"]!.AsArray()!;
 
                         foreach(var sol in marsSolKeys)
                         {
                             string solKeyString = sol.ToString();
                             int solKeyInt = int.Parse(solKeyString);
-
-                            /* if an asset sol key is not found in db keys, parses values, creates a sol object
-                            and saves it into db. If found, logs a message of not adding sol to db
                         
-                            TODO:
-                            Add measurement data validity check, if not enough data, either flag as insufficient
-                            data for the user, or do not add to db?
-                             */
-                            if(!dbSolNumbers.Contains(solKeyInt))
+                            if(solKeyInt > latestSolNumber)
+                            { 
+                                var createdSol = createSolFromJsonNode(marsWeekNode, solKeyString, solKeyInt);
+                                scopedService.Sols.Add(createdSol);
+                                scopedService.SaveChanges();
+                            } else
                             {
-                            // Temperature value parsing
-                            float averageTemperature = marsWeekNode![solKeyString]!["AT"]!["av"]!.GetValue<float>();
-                            float minimumTemperature = marsWeekNode![solKeyString]!["AT"]!["mn"]!.GetValue<float>();
-                            float maximumTemperature = marsWeekNode![solKeyString]!["AT"]!["mx"]!.GetValue<float>();
+                                _logger.LogInformation("Sol " + solKeyInt + " not added, {DateTime}", DateTime.Now);
+                            }                        
+                        }
+                    }                    
+                }
+                /* Sets the interval of the check with Timespan, can be ms/s/m/h/d
+                 in use should be relatively lengthy, at least 1h? */
+                await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+            }
+        }
+        // Override, logs a graceful CTRL-C shutdown
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("from DbUpdateService: StopAsync, Graceful Shutdown");
+            return base.StopAsync(cancellationToken);
+        }
+        private Sol createSolFromJsonNode(JsonNode node, String solKeyString, int solKeyInt)
+        {
+            // TODO: Data validity checks and according operations
 
-                            // Wind value parsing
-                            float averageSpeed = marsWeekNode![solKeyString]!["HWS"]!["av"]!.GetValue<float>();
-                            float minimumSpeed = marsWeekNode![solKeyString]!["HWS"]!["mn"]!.GetValue<float>();
-                            float maximumSpeed = marsWeekNode![solKeyString]!["HWS"]!["mx"]!.GetValue<float>();
-                            string mostCommon = marsWeekNode![solKeyString]!["WD"]!["most_common"]!["compass_point"]!.GetValue<string>();
+            // Temperature value parsing
+            float averageTemperature = node![solKeyString]!["AT"]!["av"]!.GetValue<float>();
+            float minimumTemperature = node![solKeyString]!["AT"]!["mn"]!.GetValue<float>();
+            float maximumTemperature = node![solKeyString]!["AT"]!["mx"]!.GetValue<float>();
 
-                            // Pressure value parsing
-                            float averagePressure = marsWeekNode![solKeyString]!["PRE"]!["av"]!.GetValue<float>();
-                            float minimumPressure = marsWeekNode![solKeyString]!["PRE"]!["mn"]!.GetValue<float>();
-                            float maximumPressure = marsWeekNode![solKeyString]!["PRE"]!["av"]!.GetValue<float>();
+            // Wind value parsing
+            float averageSpeed = node![solKeyString]!["HWS"]!["av"]!.GetValue<float>();
+            float minimumSpeed = node![solKeyString]!["HWS"]!["mn"]!.GetValue<float>();
+            float maximumSpeed = node![solKeyString]!["HWS"]!["mx"]!.GetValue<float>();
+            string mostCommon = node![solKeyString]!["WD"]!["most_common"]!["compass_point"]!.GetValue<string>();
 
-                            // Sol value parsing
-                            DateTime start = marsWeekNode![solKeyString]!["First_UTC"]!.GetValue<DateTime>();
-                            DateTime end = marsWeekNode![solKeyString]!["Last_UTC"]!.GetValue<DateTime>();
-                            string season = marsWeekNode![solKeyString]!["Season"]!.GetValue<string>();
+            // Pressure value parsing
+            float averagePressure = node![solKeyString]!["PRE"]!["av"]!.GetValue<float>();
+            float minimumPressure = node![solKeyString]!["PRE"]!["mn"]!.GetValue<float>();
+            float maximumPressure = node![solKeyString]!["PRE"]!["av"]!.GetValue<float>();
 
-                            // scopedService is the database context
+            // Sol value parsing
+            DateTime start = node![solKeyString]!["First_UTC"]!.GetValue<DateTime>();
+            DateTime end = node![solKeyString]!["Last_UTC"]!.GetValue<DateTime>();
+            string season = node![solKeyString]!["Season"]!.GetValue<string>();
 
-                            var createdSol = new Sol
+            var createdSol = new Sol
                             {
                                 Wind = new Wind
                                 {
@@ -115,31 +123,7 @@ namespace MarsWeatherApi
                                 Season = season,
                                 SolNumber = solKeyInt
                             };
-
-                            scopedService.Sols.Add(createdSol);
-                            scopedService.SaveChanges();
-
-                            } else
-                            {
-                                _logger.LogInformation("Sol " + solKeyInt + " not added, {DateTime}", DateTime.Now);
-                            }                        
-                        }
-
-                    }
-                    
-                }
-
-                /* Sets the interval of the check with Timespan, can be ms/s/m/h/d
-                 in use should be relatively lengthy, at least 1h? */
-                await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
-            }
-        }
-
-        // Override, logs a graceful CTRL-C shutdown
-        public override Task StopAsync(CancellationToken cancellationToken)
-        {
-            _logger.LogInformation("from DbUpdateService: StopAsync, Graceful Shutdown");
-            return base.StopAsync(cancellationToken);
+        return createdSol;                           
         }
     }
 }
