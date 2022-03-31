0e8001669fcdda4421489f27605187010dd57762
Heta Björklund <heta.bjorklund@gmail.com>
2022-03-31 15:44:59 +0300

    Changed Models to allow null values

--- Models/Temperature.cs
+++ Models/Temperature.cs
@@ -5,9 +5,9 @@ namespace MarsWeatherApi.Models
     public class Temperature
     {
         public int Id { get; set; }
-        public float Average { get; set; }
-        public float Minimum { get; set; }
-        public float Maximum { get; set; }
+        public float? Average { get; set; }
+        public float? Minimum { get; set; }
+        public float? Maximum { get; set; }
 
         [JsonIgnore]
         public Sol? Sol { get; set; }
