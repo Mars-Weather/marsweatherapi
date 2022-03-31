cea0a5214b1ae4b312a8da431b0c3aebbfdf64fc
Heta Björklund <heta.bjorklund@gmail.com>
2022-03-31 15:45:24 +0300

    New migration to allow null columns

---
+++ Migrations/20220331123805_ValuesCanBeNull.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarsWeatherApi.Migrations
+{
+    public partial class ValuesCanBeNull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Minimum",
+                table: "Winds",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Maximum",
+                table: "Winds",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Average",
+                table: "Winds",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Minimum",
+                table: "Temperatures",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Maximum",
+                table: "Temperatures",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Average",
+                table: "Temperatures",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Minimum",
+                table: "Pressures",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Maximum",
+                table: "Pressures",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Average",
+                table: "Pressures",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Minimum",
+                table: "Winds",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Maximum",
+                table: "Winds",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Average",
+                table: "Winds",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Minimum",
+                table: "Temperatures",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Maximum",
+                table: "Temperatures",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Average",
+                table: "Temperatures",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Minimum",
+                table: "Pressures",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Maximum",
+                table: "Pressures",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Average",
+                table: "Pressures",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+        }
+    }
+}
