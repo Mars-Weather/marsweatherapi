fea46fb93abfd3662248a56f03f24d4fb611a0b0
Heta Björklund <heta.bjorklund@gmail.com>
2022-04-01 12:15:54 +0300

    Added Cors

--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using MarsWeatherApi.Contexts;
 using System.Text.Json.Serialization;
-using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddHostedService<MarsWeatherApi.DbUpdateService>(); // Background service for timed DB updates
-builder.Services.AddHostedService<MarsWeatherApi.DbNasaUpdateService>(); // / Background service for timed requests to Nasa's API
+// Add services to the container
 builder.Services.AddControllers();
 
+// Add background services
+builder.Services.AddHostedService<MarsWeatherApi.DbUpdateService>(); // Background service for timed DB updates
+builder.Services.AddHostedService<MarsWeatherApi.DbNasaUpdateService>(); // / Background service for timed requests to Nasa's API
+
+// Add a reference handler
 builder.Services.AddControllers().AddJsonOptions(x =>
     x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
 
@@ -18,13 +20,24 @@ builder.Services.AddDbContext<ApplicationDbContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
     //UseInMemoryDatabase("MarsWeatherApiDB"));
 
+// Add CORS policy
+var AllowGETFromAllOrigins = "_AllowGETFromAllOrigins";
+builder.Services.AddCors(options =>
+    {
+        options.AddPolicy(name: AllowGETFromAllOrigins, builder =>
+            {
+                builder.AllowAnyOrigin().WithMethods("GET");
+            });
+    }
+);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
@@ -33,6 +46,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(AllowGETFromAllOrigins);
 
 app.UseAuthorization();
 
