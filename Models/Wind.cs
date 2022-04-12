70ecd77392d3baaead38691fdd4340eeb20f0ccc
Heta Björklund <heta.bjorklund@gmail.com>
2022-04-12 13:47:50 +0300

    Added measurement units as comments to Classes

--- Models/Wind.cs
+++ Models/Wind.cs
@@ -5,11 +5,10 @@ namespace MarsWeatherApi.Models
     public class Wind
     {
         public int Id { get; set; }
-        public float? Average { get; set; }
-        public float? Minimum { get; set; }
-        public float? Maximum { get; set; }
-        public string? MostCommonDirection { get; set; }
-
+        public float? Average { get; set; } // Metres per second, m/s
+        public float? Minimum { get; set; } // Metres per second, m/s
+        public float? Maximum { get; set; } // Metres per second, m/s
+        public string? MostCommonDirection { get; set; } // 16-wind compass rose, e.g. N for north, NNE for north-northeast
         [JsonIgnore] // JSON-annotaatioissa pitää olla Microsoftin kirjasto, ei Newtonsoftin
         public Sol? Sol { get; set; }
         public int SolId { get; set; }
