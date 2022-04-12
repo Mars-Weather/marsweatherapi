e2aac97c982cd8a0ca80a09efb853390f1f1665e
Heta Björklund <heta.bjorklund@gmail.com>
2022-04-12 13:47:50 +0300

    Added measurement units as comments to Classes

--- Models/Temperature.cs
+++ Models/Temperature.cs
@@ -5,10 +5,9 @@ namespace MarsWeatherApi.Models
     public class Temperature
     {
         public int Id { get; set; }
-        public float? Average { get; set; }
-        public float? Minimum { get; set; }
-        public float? Maximum { get; set; }
-
+        public float? Average { get; set; } // Fahrenheit
+        public float? Minimum { get; set; } // Fahrenheit
+        public float? Maximum { get; set; } // Fahrenheit
         [JsonIgnore]
         public Sol? Sol { get; set; }
         public int SolId { get; set; }
