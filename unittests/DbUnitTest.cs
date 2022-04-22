ab0497d660c9b49350ed7f18b55757f564056aaf
Heta Björklund <heta.bjorklund@gmail.com>
2022-04-22 17:19:57 +0300

    Placeholder-testit pois

--- unittests/DbUnitTest.cs
+++ unittests/DbUnitTest.cs
@@ -17,18 +17,6 @@ public class DbUnitTest
     }
 
     [Fact]
-    public void Test1()
-    {
-        Assert.True(1 == 1, "Test1 failed");
-    }
-
-    [Fact]
-    public void Test2()
-    {
-        Assert.False(1 == 2, "Test2 failed");
-    }
-
-    [Fact]
     public void DbConnectionExists()
     {
         Assert.True(_context.Database.CanConnect());
