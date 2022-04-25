649053fda2f6043c18b8578385d127d63ea71265
Heta Björklund <heta.bjorklund@gmail.com>
2022-04-25 17:24:06 +0300

    Post-testit kesken

--- integrationtests/SolControllerTest.cs
+++ integrationtests/SolControllerTest.cs
@@ -1,4 +1,10 @@
 using Xunit;
+using System;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System.Net.Http;
 using System.Collections.Generic;
 using System.Text.Json.Nodes;
@@ -11,23 +17,61 @@ public class SolControllerTest
 {
     //static readonly HttpClient client = new HttpClient();
 
-    private static readonly HttpClient client;
+    private readonly HttpClient client;
 
-    static SolControllerTest()
+    private ILogger<SolControllerTest> _logger;
+
+    /*static SolControllerTest()
     {
         client = new HttpClient();
+    }*/
+
+    public SolControllerTest()
+    {
+        client = new HttpClient();
+        //_logger = new ILogger<SolControllerTest>();
+        //_logger = logger;
+        //ILogger<SolControllerTest> logi = 
+        //ILoggerFactory facto = new ILoggerFactory();
+        //_logger = factory.CreateLogger<SolControllerTest>();
+
+        var loggerFactory = (ILoggerFactory)new LoggerFactory();
+        _logger = loggerFactory.CreateLogger<SolControllerTest>();
     }
 
     [Fact]
-    public void CorrectSolPostReturns201()
+    public void CorrectSolPostReturns201() // KESKEN
     {
         Assert.True("" == "", "CorrectSolPostReturns201 failed");
     }
 
     [Fact]
-    public void IncorrectSolPostReturns400()
+    public void IncorrectSolPostReturns400() // KESKEN
     {
         Assert.True("" == "", "IncorrectSolPostReturns");
+    }
+
+    [Fact]
+    public async void DbIsNotEmpty()
+    {
+        try
+        {
+            // Arrange
+            HttpResponseMessage response = await client.GetAsync("https://marsweather.azurewebsites.net/api/sol");
+            response.EnsureSuccessStatusCode();
+
+            // Act
+            string jsonString = await response.Content.ReadAsStringAsync();
+            JsonNode solsNode = JsonNode.Parse(jsonString)!;
+            JsonArray solList = solsNode!["$values"]!.AsArray()!;
+
+            // Assert that the returned list is not empty
+            Assert.True(solList.Count != 0, "The list was empty");
+        }   
+        catch(HttpRequestException e)
+        {
+            _logger.LogError(e.Message);
+        }  
     }
 
     [Fact]
@@ -49,11 +93,8 @@ public class SolControllerTest
         }   
         catch(HttpRequestException e)
         {
-            
+            _logger.LogError(e.Message);
         }  
-
-
-
     }
 
 
